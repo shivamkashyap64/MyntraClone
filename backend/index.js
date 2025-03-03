425c51de5eafe9e3863c1a44bfeced4efb02b4e9
Shivam Kashyap <shivamkashyap64@gmail.com>
2025-03-03 15:19:48 +0530

    4th commit

--- backend/index.js
+++ backend/index.js
@@ -39,7 +39,8 @@ app.post('/items', async (req, res) => {
   await storeItems(updatedItems);
   res.status(201).json({ message: 'Stored new item.', item: newItem });
 });
-app.listen(process.env.PORT || 4000, () => {
-  console.log(`Server is running on port ${process.env.PORT || 4000}`);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
 
